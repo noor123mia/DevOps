25c42224c6fecb5f8f7ca7ce2e6a1051f8eebd69
noor123mia <miasultan369@gmail.com>
2025-05-18 17:40:26 +0500

    Add files via upload

---
+++ HelloMavenProject/src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.maryam;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maryam! Maven project built successfully.");
+    }
+}
