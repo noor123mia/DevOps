6aac09d0c7a4ab358c22a63baf59bb532e72139d
noor123mia <miasultan369@gmail.com>
2025-05-29 12:57:46 +0500

    Delete HelloMavenProject directory

--- HelloMavenProject/src/main/java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.maryam;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maryam! Maven project built successfully.");
-    }
-}
